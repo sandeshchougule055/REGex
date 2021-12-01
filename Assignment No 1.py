746277f5facaae70ea1c9620167710b569eaacab
sandeshchougule055 <95354931+sandeshchougule055@users.noreply.github.com>
2021-12-01 18:32:13 +0530

    Add files via upload

---
+++ Assignment No 1.py
@@ -0,0 +1,22 @@
+# Name : Sandesh Suresh Chougule
+
+# Referral id : DIRSS2136
+
+#Assign four values to the values as explained in the class
+ #   (a = 45, b = 35, c = 50, d = 90)
+
+a = 45
+b = 35
+c = 50
+d = 90
+add = a+b+c+d
+print(add)
+print("*******************************************")
+sub = a-b-c-d
+print(sub)
+print("*******************************************")
+mul = a*b*c*d
+print(mul)
+print("*******************************************")
+div = a/b/c/d
+print(div)
---
+++ Assignment no 2 .py
@@ -0,0 +1,55 @@
+# Name : Sandesh Suresh Chougule
+
+# Referral id : DIRSS2136
+
+#1. Take each and every letter of your name in separate variable and try to print ASCII Value of
+#each and every letter       Using ord() function
+
+A=ord("S")
+B=ord("a")
+C=ord("n")
+D=ord("d")
+E=ord("e")
+H=ord("s")
+G=ord("h")
+print("S code :",A)
+print("a code :",B)
+print("n code :",C)
+print("d code :",D)
+print("e code :",E)
+print("s code :",H)
+print("h code :",G)
+
+print("*************************************************")
+
+#2. Find the ASCII Number of word “iND@IA”
+#“i”,"N","D","@","I","A"
+
+a=ord("i")
+b=ord("N")
+c=ord("D")
+d=ord("@")
+e=ord("I")
+f=ord("A")
+print("i character is :",a)
+print("N character is :",b)
+print("D character is :",c)
+print("@ character is :",d)
+print("I character is :",e)
+print("A character is :",f)
+
+print("*************************************************")
+
+#3. Find the respective characters of following ASCII numbers ( use chr() )
+# 80,92,99,106,116
+
+a = chr(80)
+b = chr(92)
+c = chr(99)
+d = chr(106)
+e = chr(116)
+print("80 character is : ",a)
+print("92 character is : ",b)
+print("99 character is : ",c)
+print("106 character is : ",d)
+print("116 character is : ",e)
